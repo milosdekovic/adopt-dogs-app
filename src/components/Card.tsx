a7af03cb25d9c7f611304bbd587254365e70bd5f
milosdekovic <93866192+milosdekovic@users.noreply.github.com>
2024-04-22 21:33:54 +0200

    add dog api

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,30 +1,86 @@
-import { Card, Image, Text, Badge, Button, Group } from "@mantine/core";
+import { Card, Image, Text, Button, Group, Loader } from "@mantine/core";
+import { useEffect, useState } from "react";
+
+const BASE_URL = "https://dog.ceo/api/breeds/image/random";
+
+interface Pet {
+  message: string;
+  status: string;
+}
 
 const CardPage = () => {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [pets, setPets] = useState<Pet[]>([]);
+
+  useEffect(() => {
+    const fetchPets = async () => {
+      try {
+        const petsData = [];
+        for (let i = 0; i < 10; i++) {
+          // Change this number to fetch the number of pets you want
+          const response = await fetch(BASE_URL);
+          const data = await response.json();
+          petsData.push(data);
+        }
+        setPets(petsData);
+        setLoading(false); // Set loading to false once data is fetched
+      } catch (error) {
+        setLoading(false); // Set loading to false if there's an error
+        alert("Oops, there was an error fetching the pets! Please try again.");
+      }
+    };
+
+    fetchPets();
+  }, []);
+
   return (
-    <Card shadow="sm" padding="lg" radius="md" withBorder>
-      <Card.Section>
-        <Image
-          src="https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/images/bg-8.png"
-          height={160}
-          alt="Norway"
-        />
-      </Card.Section>
+    <div className="grid grid-cols-2 gap-8 mx-auto">
+      {loading ? ( // If loading is true, display the spinner
+        <Loader size="lg" />
+      ) : (
+        pets.map((pet, index) => {
+          const breedName = pet.message.split("/")[4]; // Extract breed name from image URL
+          const formatBreedName = breedName
+            .split("-")
+            .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+            .join(" ");
 
-      <Group justify="space-between" mt="md" mb="xs">
-        <Text fw={500}>Norway Fjord Adventures</Text>
-        <Badge color="pink">On Sale</Badge>
-      </Group>
+          return (
+            <Card
+              className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 duration-300"
+              key={index}
+              shadow="sm"
+              padding="lg"
+              radius="md"
+              withBorder
+            >
+              <Card.Section>
+                <Image
+                  className="w-full h-[250px] object-cover"
+                  width={250}
+                  height={250}
+                  src={pet.message}
+                />
+              </Card.Section>
 
-      <Text size="sm" c="dimmed">
-        With Fjord Tours you can explore more of the magical fjord landscapes
-        with tours and activities on and around the fjords of Norway
-      </Text>
+              <Group justify="space-between" mt="md" mb="xs">
+                <Text fw={500}>{formatBreedName}</Text>
+                {/* Display the breed name */}
+              </Group>
 
-      <Button color="blue" fullWidth mt="md" radius="md">
-        Book classic tour now
-      </Button>
-    </Card>
+              <Text size="sm" c="dimmed">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga,
+                itaque.
+              </Text>
+
+              <Button color="blue" fullWidth mt="md" radius="md">
+                Adopt
+              </Button>
+            </Card>
+          );
+        })
+      )}
+    </div>
   );
 };
 
--- src/pages/Paws.tsx
+++ src/pages/Paws.tsx
@@ -1,40 +1,9 @@
-import { Image } from "@mantine/core";
-import { useEffect, useState } from "react";
-
-const BASE_URL = "https://dog.ceo/api/breeds/image/random";
-
-interface Pet {
-  message: string;
-  status: string;
-}
+import CardPage from "../components/Card";
 
 const Paws = () => {
-  const [pets, setPets] = useState<Pet[]>([]);
-
-  useEffect(() => {
-    const fetchPets = async () => {
-      try {
-        const response = await fetch(BASE_URL);
-        const data = await response.json();
-        setPets([data]);
-      } catch (error) {
-        alert("Oops, there was an error fetching the pets! Please try again.");
-      }
-    };
-
-    fetchPets();
-  }, []);
-
   return (
-    <div className="grid container gap-5">
-      <h1>Paws home page</h1>
-      <ul className="gap-10">
-        {pets.map((pet) => (
-          <li key={pet.status}>
-            <Image src={pet.message}></Image>
-          </li>
-        ))}
-      </ul>
+    <div className="grid container gap-5 mx-auto mt-5 mb-5">
+      <CardPage />
     </div>
   );
 };
