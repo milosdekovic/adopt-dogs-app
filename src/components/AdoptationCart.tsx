ab96574f66bd99f03309f6ca6ff018cf6d02f858
milosdekovic <93866192+milosdekovic@users.noreply.github.com>
2024-04-24 22:53:26 +0200

    clean code

--- src/components/AdoptationCart.tsx
+++ src/components/AdoptationCart.tsx
@@ -1,15 +1,14 @@
-import { Button, Divider, Drawer, Image, Indicator } from "@mantine/core";
-import { IconMinus, IconPawFilled } from "@tabler/icons-react";
+import { Button, Drawer, Indicator } from "@mantine/core";
+import { IconPawFilled } from "@tabler/icons-react";
 import { useAdoptation } from "../context/AdoptationContext";
 import { useDisclosure } from "@mantine/hooks";
 import { useState } from "react";
+import CartItem from "./CartItem";
 
 const AdoptationCart = () => {
   const [opened, { open, close }] = useDisclosure(false);
-  const { cartQuantity, getCartItems, removeFromCart, clearCart } =
-    useAdoptation();
-  const cartItems = getCartItems();
-  const [message, setMessage] = useState("You have not rescued a doggo yet 😞");
+  const { cartQuantity } = useAdoptation();
+  const [, setMessage] = useState("You have not rescued a doggo yet 😞");
 
   return (
     <div>
@@ -22,80 +21,8 @@ const AdoptationCart = () => {
         }}
         overlayProps={{ backgroundOpacity: 0.8 }}
       >
-        <h1 className="font-bold text-2xl mb-1">Your doggos: </h1>
-        <Divider />
-        <div className="mb-5">
-          {cartQuantity > 0 ? (
-            <div className="mt-5 grid gap-5">
-              {cartItems.map((item, index) => {
-                const formatBreedName = item.pet.message.split("/")[4];
-                const breedName = formatBreedName
-                  .split("-")
-                  .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-                  .join(" ");
-                return (
-                  <>
-                    <div className="flex justify-between items-end" key={index}>
-                      <div>
-                        <div className="flex gap-5 items-center">
-                          <Image
-                            className="min-w-[120px] rounded-lg"
-                            src={item.pet.message}
-                            style={{
-                              objectFit: "cover",
-                              width: "120px",
-                              height: "100px",
-                            }}
-                          />
-
-                          <p className="font-bold">{breedName}</p>
-                        </div>
-                      </div>{" "}
-                      <div className="my-auto">
-                        <Button
-                          className="w-[45px]"
-                          onClick={() => removeFromCart(item.index)}
-                          variant="filled"
-                          color="red"
-                          size="xs"
-                          radius="lg"
-                        >
-                          <IconMinus />
-                        </Button>
-                      </div>
-                    </div>
-                  </>
-                );
-              })}
-              <Divider />
-              <div className="flex justify-between">
-                <Button
-                  color="#1F1F1F"
-                  radius="md"
-                  onClick={() => {
-                    clearCart();
-                    setMessage(
-                      `Thank you for adopting ${cartQuantity} doggo(s)!`,
-                    );
-                  }}
-                >
-                  Adopt {cartQuantity} doggo(s)
-                </Button>
-                <Button
-                  color="#1F1F1F"
-                  radius="md"
-                  onClick={() => {
-                    clearCart();
-                    setMessage("You have not rescued a doggo yet 😞");
-                  }}
-                >
-                  Remove all
-                </Button>
-              </div>
-            </div>
-          ) : (
-            <p className="mt-5 text-center">{message}</p>
-          )}
+        <div>
+          <CartItem />
         </div>
       </Drawer>
       <Button
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ import AdoptationCart from "./AdoptationCart";
 const Navbar = () => {
   return (
     <nav className="flex bg-[#242424] sticky top-0 z-10 justify-between p-5 font-semibold text-white shadow-md">
-      <div className="flex justify-between mx-auto w-full max-w-5xl items-center">
+      <div className="flex justify-between mx-auto w-full max-w-5xl">
         <Link to={"/"}>
           <h1 className="lg:text-3xl text-2xl">Adopt Doggo</h1>
         </Link>
